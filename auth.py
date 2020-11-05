ce66338bdea4780c57cd6fd935a4e3b6ad0c347e
DanielaMazur <danielamazur2000@gmail.com>
2020-11-05 14:17:05 +0200

    send auth email via sendGrid API

---
+++ auth.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, request
+
+import os
+from sendgrid.helpers.mail import Mail
+from sendgrid import SendGridAPIClient
+
+auth = Blueprint('auth', __name__, url_prefix='/auth')
+
+FROM_EMAIL = 'beepboop.faf@gmail.com'
+
+TEMPLATE_ID = 'd-6b6d8549c1774c7688d71520cbb9136b'
+
+@auth.route('/')
+def postEmail(): 
+  email = request.args.get('email')
+
+  # create Mail object
+  message = Mail(
+        from_email=FROM_EMAIL,
+        to_emails=[email])
+
+  # pass custom values for HTML placeholders
+  message.dynamic_template_data = {
+      'verification_code': 123456
+  }
+
+  message.template_id = TEMPLATE_ID
+
+  try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+
+  except Exception as e:
+    print("Error: {0}".format(e))
+  return str(response.status_code)
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def index():
+import auth
 
-	return "Hello, World!"
+def create_app():
+	app = Flask(__name__)
 
-if __name__ == "__main__":
-	app.run(debug=True)
+	app.register_blueprint(auth.auth)
+
+	if __name__ == "__main__":
+		app.run(debug=True)
+
+	return app
