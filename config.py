2745a0597eeacfee07804938ef664442a491987e
dimatrubca <dimatrubca@gmail.com>
2020-12-03 19:44:51 +0200

    Add config file

---
+++ config.py
@@ -0,0 +1,10 @@
+
+import os
+
+
+class Config:
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'beep-boop')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///beepboop.db')
+
+    FROM_EMAIL = 'beepboop.faf@gmail.com'
+    TIME_CODE_IS_VALID = 300 #code will be valid 5 min
--- server.py
+++ server.py
@@ -2,38 +2,27 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 from dotenv import load_dotenv
 
-import auth
+db = SQLAlchemy()
+migrate = Migrate()
 
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///beepboop.db'
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-from tables import User, User_Courses, Teacher, Teacher_Courses, Courses, Course_Questions, Course_Question_Answer, Course_Materials, Message_History
-
-
-def create_app():
+def create_app(config_class=Config):
+	app = Flask(__name__)
+	app.config.from_object(Config)
 	
-	app.config['JWT_SECRET_KEY'] = 'beep-boop'
-	
-	
-	JWTManager(app)
-
-
-
-
 	load_dotenv('./.env')
+
+	db.init_app(app)
+	migrate.init_app(app, db)
+
+	JWTManager(app)
 	CORS(app)
 
+	import auth
 	app.register_blueprint(auth.auth)
-
-	if __name__ == "__main__":
-		app.run(debug=True)
 
 	return app
